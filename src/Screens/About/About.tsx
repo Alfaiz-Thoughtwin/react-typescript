078fabe189d7461fa3e8a504606cf6cabe2e17d0
Alfaiz.Thoughtwin <alfaiz.thoughtwin@gmail.com>
2021-10-20 13:48:25 +0530

    Didn't Know? Why I Am Commit This git status!

--- src/Screens/About/About.tsx
+++ src/Screens/About/About.tsx
@@ -1,12 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { CountryDropdown, RegionDropdown, CountryRegionData } from 'react-country-region-selector';
 import { Navbar } from '../../Components';
 
 export const About = () => {
+
+    const [country, setCountry] = useState<any | null>("");
+    const [region, setRegion] = useState<any | null>("");
+
+    const selectCountry = (val:any) => {
+        setCountry(val);
+    }
+    
+    const selectRegion = (val:any) => {
+        setRegion(val);
+    }
+
     return (
         <>
             <Navbar />
             <div className="container text-center my-4">
                 <h1>About Page</h1>
+            </div>
+
+            <div className="container text-center my-4">
+                <CountryDropdown
+                    defaultOptionLabel="Select a country"
+                    value={country}
+                    onChange={(val:any) => selectCountry(val)} />
+
+                <RegionDropdown
+                    blankOptionLabel="No country selected"
+                    defaultOptionLabel="Now select a region"
+                    country={country}
+                    value={region}
+                    onChange={(val:any) => selectRegion(val)} />
             </div>
         </>
     );
